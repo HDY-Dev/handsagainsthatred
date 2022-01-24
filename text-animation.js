2c609195bbeb344156a449b5afd53c40ca0e3297
HDY-Agency <digital@hdyagency.com>
2022-01-24 14:57:43 +0000

    compressed gifs

--- text-animation.js
+++ text-animation.js
@@ -48,21 +48,21 @@ gsap.to('.hero-tagline',{
 
 
 gsap.to('#intro-text-1',{
-    y:'20%',rotate:0,duration:1,scrollTrigger:{
+    y:'30%',rotate:0,duration:1,scrollTrigger:{
       trigger:'#intro-text-1',
       scrub:true,
       start:'top 50%',
-      ease: 'elastic'
+      ease: 'back.inOut(1.7)'
     }
   })
 
 
 gsap.to('#intro-image-1',{
-    y:'-20%',rotate:0,duration:1,scrollTrigger:{
+    y:'-30%',rotate:0,duration:1,scrollTrigger:{
       trigger:'#intro-image-1',
       scrub:true,
       start:'top 50%',
-      ease: 'elastic'
+      ease: 'back.inOut(1.7)'
     }
   })
 
@@ -71,7 +71,7 @@ gsap.to('#intro-image-1',{
       trigger:'#intro-text-2',
       scrub:true,
       start:'top 50%',
-      ease: 'elastic'
+      ease: 'back.inOut(1.7)'
     }
   })
 
@@ -81,6 +81,6 @@ gsap.to('#intro-image-2',{
       trigger:'#intro-image-2',
       scrub:true,
       start:'top 50%',
-      ease: 'elastic'
+      ease: 'back.inOut(1.7)'
     }
   })
