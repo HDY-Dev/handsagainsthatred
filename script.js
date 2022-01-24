f505c1e2091ac344681d8f2b76cfb8879ad52657
HDY-Agency <digital@hdyagency.com>
2022-01-24 14:43:31 +0000

    major changes
    animations, gifs the lot all added

--- script.js
+++ script.js
@@ -22,3 +22,45 @@ $(document).ready(function(){
     } // End if
   });
 });
+
+
+let line1 = document.getElementById('title1');
+
+line1.addEventListener("mouseover", function(event) {
+    event.target.style.color = "yellow";
+}, false);
+
+line1.addEventListener("mouseout", function(event) {
+  event.target.style.color = "gray";
+}, false);
+
+
+let line2 = document.getElementById('title2');
+
+line2.addEventListener("mouseover", function(event) {
+    event.target.style.color = "yellow";
+}, false);
+
+line2.addEventListener("mouseout", function(event) {
+  event.target.style.color = "gray";
+}, false);
+
+let line3 = document.getElementById('title3');
+
+line3.addEventListener("mouseover", function(event) {
+    event.target.style.color = "yellow";
+}, false);
+
+line3.addEventListener("mouseout", function(event) {
+  event.target.style.color = "gray";
+}, false);
+
+let line4 = document.getElementById('title4');
+
+line4.addEventListener("mouseover", function(event) {
+    event.target.style.color = "yellow";
+}, false);
+
+line4.addEventListener("mouseout", function(event) {
+  event.target.style.color = "gray";
+}, false);
---
+++ text-animation.js
@@ -0,0 +1,86 @@
+gsap.to('.title-1',{
+    x:'-50%',rotate:0,duration:3,scrollTrigger:{
+      trigger:'.hero-tagline',
+      scrub:true,
+      start:'top 40%',
+      ease: 'elastic'
+    }
+  })
+
+  gsap.to('.title-2',{
+    x:'50%',rotate:0,duration:3,scrollTrigger:{
+      trigger:'.hero-tagline',
+      scrub:true,
+      start:'top 40%',
+      ease: 'elastic'
+    }
+  })
+
+  gsap.to('.title-3',{
+    x:'-50%',rotate:0,duration:3,scrollTrigger:{
+      trigger:'.hero-tagline',
+      scrub:true,
+      start:'top 40%',
+      ease: 'elastic'
+    }
+  })
+
+  gsap.to('.title-4',{
+    x:'50%',rotate:0,duration:3,scrollTrigger:{
+      trigger:'.hero-tagline',
+      scrub:true,
+      start:'top 40%',
+      ease: 'elastic'
+    }
+  })
+
+
+
+gsap.to('.hero-tagline',{
+    x:'-50%',rotate:0,duration:3,scrollTrigger:{
+      trigger:'.hero-tagline',
+      scrub:true,
+      start:'top 40%',
+      ease: 'elastic'
+    }
+  })
+
+
+
+gsap.to('#intro-text-1',{
+    y:'20%',rotate:0,duration:1,scrollTrigger:{
+      trigger:'#intro-text-1',
+      scrub:true,
+      start:'top 50%',
+      ease: 'elastic'
+    }
+  })
+
+
+gsap.to('#intro-image-1',{
+    y:'-20%',rotate:0,duration:1,scrollTrigger:{
+      trigger:'#intro-image-1',
+      scrub:true,
+      start:'top 50%',
+      ease: 'elastic'
+    }
+  })
+
+  gsap.to('#intro-text-2',{
+    y:'20%',rotate:0,duration:1,scrollTrigger:{
+      trigger:'#intro-text-2',
+      scrub:true,
+      start:'top 50%',
+      ease: 'elastic'
+    }
+  })
+
+
+gsap.to('#intro-image-2',{
+    y:'-20%',rotate:0,duration:1,scrollTrigger:{
+      trigger:'#intro-image-2',
+      scrub:true,
+      start:'top 50%',
+      ease: 'elastic'
+    }
+  })
