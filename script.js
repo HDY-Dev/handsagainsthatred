aa5f49e5326357dac5231659062d1c9326ab54a3
HDY-Agency <digital@hdyagency.com>
2022-01-27 16:48:53 +0000

    side scrolling attempts
    still needs looking at

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+var loading;
+
+function preloader() {
+  loading = setTimeout(showPage, 4000);
+}
+
+function showPage() {
+  document.getElementById('loading-block').style.display = "none";
+  document.getElementById('page-wrapper').style.display = "block";
+  document.getElementById('navigation').style.display = "block";
+}
+
 $(document).ready(function(){
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
@@ -24,72 +36,18 @@ $(document).ready(function(){
 });
 
 
-let line1 = document.getElementById('title1');
-
-line1.addEventListener("mouseover", function(event) {
-    event.target.style.color = "yellow";
-}, false);
-
-line1.addEventListener("mouseout", function(event) {
-  event.target.style.color = "gray";
-}, false);
-
-
-let line2 = document.getElementById('title2');
-
-line2.addEventListener("mouseover", function(event) {
-    event.target.style.color = "yellow";
-}, false);
-
-line2.addEventListener("mouseout", function(event) {
-  event.target.style.color = "gray";
-}, false);
-
-let line3 = document.getElementById('title3');
-
-line3.addEventListener("mouseover", function(event) {
-    event.target.style.color = "yellow";
-}, false);
-
-line3.addEventListener("mouseout", function(event) {
-  event.target.style.color = "gray";
-}, false);
-
-let line4 = document.getElementById('title4');
-
-line4.addEventListener("mouseover", function(event) {
-    event.target.style.color = "yellow";
-}, false);
-
-line4.addEventListener("mouseout", function(event) {
-  event.target.style.color = "gray";
-}, false);
-
-var timeout;
-
-function preloader() {
-  timeout = setTimeout(showPage, 3000);
-};
-
-
-function showPage() {
-  document.getElementById('loading-block').style.display = "none";
-  document.getElementById('page-wrapper').style.display = "block";
-};
-
-
-const content = document.querySelector('.skew-scroll');
-
-
+const content = document.getElementsByClassName('skew-scroll');
 
 let currentPos = window.pageXOffset;
 
 const update = () => {
   const newPos = window.pageYOffset;
   const diff = newPos - currentPos;
-  const speed = diff * 0.35;
+  const speed = diff * 0.20;
 
-  content.style.transform = `skewY(${ speed }deg)`;
+  for (var i = 0; i < content.length; i++){
+    content[i].style.transform = `skewY(${ speed }deg)`;
+  }
   
   currentPos = newPos;
 
@@ -100,8 +58,6 @@ const update = () => {
 update();
 
 contentX = document.getElementsByClassName('side-skew-scroll');
-
-
 
 let currentPos2 = window.pageXOffset;
 
@@ -121,32 +77,3 @@ const update2 = () => {
 
 
 update2();
-
-
-function animateValue(obj, start, end, duration) {
-  let startTimestamp = null;
-  const step = (timestamp) => {
-    if (!startTimestamp) startTimestamp = timestamp;
-    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-    obj.innerHTML = Math.floor(progress * (start - end) + end);
-
-    if (progress < 1) {
-      window.requestAnimationFrame(step);
-    }
-  };
-  window.requestAnimationFrame(step);
-}
-
-var loading;
-
-function preloader() {
-  loading = setTimeout(showPage, 4000);
-}
-
-function showPage() {
-  document.getElementById('loading-block').style.display = "none";
-  document.getElementById('page-wrapper').style.display = "block";
-  document.querySelector('header').style.display = "block";
-}
-
-
