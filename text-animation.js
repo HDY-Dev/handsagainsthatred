a667942cfec93077965bf317c593ad2f90a06696
HDY-Agency <digital@hdyagency.com>
2022-01-27 13:32:57 +0000

    changes

--- text-animation.js
+++ text-animation.js
@@ -1,88 +1,41 @@
-// gsap.to('.title-1',{
-//     x:'-50%',rotate:0,duration:0.85,scrollTrigger:{
-//       trigger:'.hero-tagline',
-//       scrub:true,
-//       start:'top 40%',
-//       ease: 'elastic'
-//     }
-//   })
+// var windowWidth = window.innerWidth;
 
-//   gsap.to('.title-2',{
-//     x:'50%',rotate:0,duration:0.85,scrollTrigger:{
-//       trigger:'.hero-tagline',
-//       scrub:true,
-//       start:'top 40%',
-//       ease: 'elastic'
-//     }
-//   })
+// if (windowWidth > 992) {
 
-//   gsap.to('.title-3',{
-//     x:'-50%',rotate:0,duration:0.85,scrollTrigger:{
-//       trigger:'.hero-tagline',
-//       scrub:true,
-//       start:'top 40%',
-//       ease: 'elastic'
-//     }
-//   })
-
-//   gsap.to('.title-4',{
-//     x:'50%',rotate:0,duration:0.85,scrollTrigger:{
-//       trigger:'.hero-tagline',
-//       scrub:true,
-//       start:'top 40%',
-//       ease: 'elastic'
-//     }
-//   })
-
-
-
-// gsap.to('.hero-tagline',{
-//     x:'-50%',rotate:0,duration:0.85,scrollTrigger:{
-//       trigger:'.hero-tagline',
-//       scrub:true,
-//       start:'top 40%',
-//       ease: 'elastic'
-//     }
-//   })
-
-
-
-// gsap.to('#intro-text-1',{
-//     y:'30%',rotate:0,duration:1,scrollTrigger:{
-//       trigger:'#intro-text-1',
-//       scrub:1,
-//       start:'top 50%',
-//       ease: 'back.inOut(1.7)'
-//     }
-//   })
-
-
-if (window.innerWidth > 992) {
   gsap.to('#intro-image-1',{
-    y:'-30%',rotate:0,duration:1,scrollTrigger:{
+    y:'-20%',rotate:0,duration:1,scrollTrigger:{
       trigger:'#intro-image-1',
       scrub:1,
-      start:'top 50%',
+      start:'top 20%',
+      ease: 'back.inOut(1.7)'
+    }
+  })
+
+  gsap.to('#intro-text-1',{
+    y:'20%',rotate:0,duration:1,scrollTrigger:{
+      trigger:'#intro-text-1',
+      scrub:1,
+      start:'top 20%',
       ease: 'back.inOut(1.7)'
     }
   })
 
   gsap.to('#intro-text-2',{
-    y:'-20%',rotate:0,duration:1,scrollTrigger:{
+    y:'20%',rotate:0,duration:1,scrollTrigger:{
       trigger:'#intro-text-2',
       scrub:true,
-      start:'top 50%',
+      start:'top 20%',
       ease: 'back.inOut(1.7)'
     }
   })
 
 
-gsap.to('#intro-image-2',{
-    y:'20%',rotate:0,duration:1,scrollTrigger:{
+  gsap.to('#intro-image-2',{
+    y:'-20%',rotate:0,duration:1,scrollTrigger:{
       trigger:'#intro-image-2',
       scrub:true,
-      start:'top 50%',
+      start:'top 20%',
       ease: 'back.inOut(1.7)'
     }
   })
-}
+// }
